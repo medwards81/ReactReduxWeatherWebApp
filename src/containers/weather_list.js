d54a90c01b31caeba2015eaa113c1afeb7c73bb1
Marc Edwards <mdouglasedwards@gmail.com>
2017-08-04 22:32:26 -0400

    Added google maps api component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,24 +1,27 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
-		const temps = cityData.list.map(weather => weather.main.temp); // in kelvin
+		const temps = _map.(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const { lon, lat } = cityData.city.coord;
 
 		return (
 			<tr key={cityData.city.id}>
-				<td>{cityData.city.name}</td>
+				<td><GoogleMap lon={lon} lat={lat} /></td>
 				<td>
-					<Chart height={120} width={180} data={temps} color='orange' />
+					<Chart height={120} width={180} data={temps} color='orange' units='k' />
 				</td>
 				<td>
-					<Chart height={120} width={180} data={humidities} color='green' />
+					<Chart height={120} width={180} data={humidities} color='green' units='%' />
 				</td>
 				<td>
-					<Chart height={120} width={180} data={pressures} color='black' />
+					<Chart height={120} width={180} data={pressures} color='black' units='hPa' />
 				</td>
 			</tr>
 		);
@@ -30,9 +33,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Humidity</th>
-						<th>Pressure</th>
+						<th>Temperature (C)</th>
+						<th>Humidity (%)</th>
+						<th>Pressure (hPa)</th>
 					</tr>
 				</thead>
 				<tbody>
